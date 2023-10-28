6a8c74d14517e3a225ef5a103ade85e553ce61f0
Torrax <Carson.morris2@gmail.com>
2023-10-28 04:08:05 -0400

    Update __init__.py

--- music_assistant/server/providers/aux/__init__.py
+++ music_assistant/server/providers/aux/__init__.py
@@ -70,7 +70,6 @@ class AUXProvider(MusicProvider):
                 )
             },
         )
-        await self.radio_controller.add_item_to_library(radio)
 
     async def get_audio_stream(self, streamdetails: StreamDetails) -> AsyncGenerator[bytes, None]:
         """Return the audio stream for the AUX provider item."""
